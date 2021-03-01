a44ce2f66e6769f171dde3c924012acad724d553
Josh Frank <eurosewusa@gmail.com>
2021-03-01 16:42:55 -0500

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import APIUrl from "./APIUrl";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 
 function App() {
+
+  const [ stocks, setStocks ] = useState( [] );
+  const [ portfolio, setPortfolio ] = useState( [] );
+  const [ sortAndFilter, setSortAndFilter ] = useState( { sort: "", filter: "" } );
+
+  useEffect( () => {
+    fetch( APIUrl ).then( response => response.json() ).then( setStocks );
+  }, [] );
+
+  const sortedAndFilteredStocks = stocks.filter( stock => stock.type.includes( sortAndFilter.filter ) ).sort( ( thisStock, thatStock ) => sortAndFilter.sort === "Alphabetically" ? thisStock.name.localeCompare( thatStock.name ) : sortAndFilter.sort === "Price" ? thisStock.price - thatStock.price : -1 );
+
+  // console.log( sortedAndFilteredStocks );
+
   return (
     <div>
       <Header />
-      <MainContainer />
+      <MainContainer
+        allStocks={ stocks }
+        portfolio={ portfolio }
+        setPortfolio={ setPortfolio }
+        sortAndFilter={ sortAndFilter }
+        setSortAndFilter={ setSortAndFilter }
+        sortedAndFilteredStocks={ sortedAndFilteredStocks }
+      />
     </div>
   );
+
 }
 
 export default App;
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer( { portfolio, setPortfolio } ) {
+
+  function removeFromPortfolio( stockId ) {
+    const updatedPortfolio = portfolio.filter( portfolioStock => portfolioStock.id !== stockId );
+    setPortfolio( updatedPortfolio );
+  }
+
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      { portfolio.map( stock => <Stock key={ stock.id } stock={ stock } portfolio={ portfolio } setPortfolio={ setPortfolio } clickCallback={ removeFromPortfolio }/> ) }
     </div>
   );
+
 }
 
 export default PortfolioContainer;
